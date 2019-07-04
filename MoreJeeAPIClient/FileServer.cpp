3c60d4b5091abc827c44b2a9742fa5e70385fa91
leon <liwang.pu@gmail.com>
2019-07-04 20:09:23 +0800

    2

---
+++ MoreJeeAPIClient/FileServer.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "FileServer.h"
+
+
+FileServer::FileServer()
+{
+}
+
+
+FileServer::~FileServer()
+{
+}
--- MoreJeeAPIClient/Startup.cpp
+++ MoreJeeAPIClient/Startup.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include "Startup.h"
+#include "include/Startup.h"
 
 //namespace MoreJeeAPI
 //{
