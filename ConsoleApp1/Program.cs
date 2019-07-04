2258e2dcdc6742c94ad00d2d80fa347f7c513ee5
leon <liwang.pu@gmail.com>
2019-07-04 18:42:19 +0800

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder build = new StringBuilder();
+
+            for (long i = 0; i < 20000000000; i++)
+            {
+                build.Append("1");
+            }
+
+
+
+        }
+    }
+}
